b99db5d633a6f71755eeb9fd05bdbb6f914da3a0
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-07-18 11:32:47 +0100

    update

--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -22,6 +22,7 @@ export default function Parent() {
   return (
     <div>
       <Child name={name} pats={pats} handleBobsPats={handleBobsPats} />
+      <Child name={"Peter"} />
     </div>
   );
 }
