0596e80192930f8673372cbbb2d13459907bf701
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-07-18 19:08:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import "./App.css";
 
 import Parent from "./components/Parent/Parent";
 
+import data from "./data.json";
+
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>{data[0].name}</li>
+        <li>{data[1].name}</li>
+        <li>{data[2].name}</li>
+      </ul>
       <h1>Props and State</h1>
       <Parent />
     </div>
