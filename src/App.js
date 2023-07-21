7a6ae28d1eb93dccd13df2a86cb66de5133978e8
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-07-21 09:18:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
+// import "./Reset.css";
 import "./App.css";
 
-import Parent from "./components/Parent/Parent";
+import { useState } from "react";
 
-import data from "./data.json";
+import Parent from "./components/Parent/Parent";
+import Header from "./components/Header/Header";
+import Modal from "./components/Modal/Modal";
+
+// import data from "./data.json";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+    console.log(showModal);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  function pointless(experience) {
+    console.log(`This is not pointless, it's ${experience}!`);
+  }
+
   return (
     <div className="App">
-      <ul>
+      {/* <ul>
         <li>{data[0].name}</li>
         <li>{data[1].name}</li>
         <li>{data[2].name}</li>
-      </ul>
-      <h1>Props and State</h1>
+      </ul> */}
+      {/* <h1>Props and State</h1> */}
+      <Header openModal={openModal} />
       <Parent />
+      {showModal && <Modal closeModal={closeModal} nick={"great"} pointless={pointless} />}
     </div>
   );
 }
--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -22,7 +22,7 @@ export default function Parent() {
   return (
     <div>
       <Child name={name} pats={pats} handleBobsPats={handleBobsPats} />
-      <Child name={"Peter"} />
+      {/* <Child name={"Peter"} /> */}
     </div>
   );
 }
